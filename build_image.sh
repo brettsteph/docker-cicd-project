283090d446d35b5c13e12c3a1b4a305a032ef616
Brett Stephen <igadget777@gmail.com>
2023-02-23 02:55:46 +0000

    Update variable

--- build_image.sh
+++ build_image.sh
@@ -4,4 +4,4 @@
 set eu
 
 # build the docker image
-docker build -f ./$DOCKER_HUB_REPO_NAME/Dockerfile -t $DOCKER_HUB_REPO_NAME:latest .
+docker build -f ./$DOCKER_IMAGE_TAG/Dockerfile -t $DOCKER_IMAGE_TAG .
