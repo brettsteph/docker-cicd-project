b2158e9acd89a532fa21fa7d7b88e51e8f044da5
brettsteph <cloudengineer777@gmail.com>
2023-02-24 12:55:24 -0500

    Add docker login

--- build_image.sh
+++ build_image.sh
@@ -3,5 +3,8 @@
 # Fail on any error
 set eu
 
+# login to your docker hub account
+docker login --username $DOCKER_HUB_USERNAME --password $DOCKER_HUB_PASSWORD
+
 # build the docker image (tage-name=folder-name/Dockerfile)
 docker build -f ./$DOCKER_IMAGE_TAG/Dockerfile -t $DOCKER_IMAGE_TAG .
