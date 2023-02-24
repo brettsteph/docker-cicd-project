51a818a91d014ce345c3624347a8837e55403437
brettsteph <cloudengineer777@gmail.com>
2023-02-24 12:08:57 -0500

    Add comment

--- build_image.sh
+++ build_image.sh
@@ -3,5 +3,5 @@
 # Fail on any error
 set eu
 
-# build the docker image
+# build the docker image (tage-name=folder-name/Dockerfile)
 docker build -f ./$DOCKER_IMAGE_TAG/Dockerfile -t $DOCKER_IMAGE_TAG .
